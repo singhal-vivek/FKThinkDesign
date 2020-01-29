a9646e431ed0c7f550d1065c46d355540155abff
singhal-vivek <vivek23399@gmail.com>
2020-01-29 17:55:21 +0530

    2.1

---
+++ Main_1.java
@@ -0,0 +1,13 @@
+class Main_1 {
+	public static void main(String[] args) {
+		int chainring = 52;
+		int cog = 11;
+		float ratio = (float)chainring / cog;
+		System.out.println(ratio);
+
+		chainring = 30;
+		cog = 27;
+		ratio = (float)chainring / cog;
+		System.out.println(ratio);
+	}
+}
